08fc72be7c24a8d703c8426b16f0b7f98d2ec47c
Sh4dy01 <maestraccihugofacebook@yahoo.fr>
2022-03-28 13:49:02 +0200

    phaser integration

---
+++ app.js
@@ -0,0 +1,19 @@
+const config = {
+  width: 800,
+  height: 400,
+  type: Phaser.AUTO
+}
+
+var game = new Phaser.Game(config)
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update() {
+
+}
