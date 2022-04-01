10e4f4eaed8302a81ab170f3924cfdbd30afd81c
Gotvna <nvogt@gaming.tech>
2022-04-01 09:21:31 +0200

    add commentary

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import StartScene from "./scenes/startScene";
 import SecondScene from "./scenes/secondScene";
 import Game from "./Game";
 
+
+// Initialize Phaser
 const config = {
     type: Phaser.AUTO,
     width: 800,
@@ -30,8 +32,8 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-game.scene.add('Game', Game);
-game.scene.add('StartScene', StartScene);
-game.scene.add('SecondScene', SecondScene);
+game.scene.add('Game', Game); // Add the Game scene
+game.scene.add('StartScene', StartScene); // Add the StartScene scene
+game.scene.add('SecondScene', SecondScene); // Add the SecondScene scene
 
-game.scene.start('StartScene');
+game.scene.start('StartScene'); // Start the StartScene scene
