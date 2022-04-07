93470a347abbaec21ad0b172c00b6e779fe43656
Gotvna <nvogt@gaming.tech>
2022-04-07 23:27:00 +0200

    update

---
+++ assets/tiledmap/Levels/wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="wall" tilewidth="91" tileheight="161" tilecount="6" columns="6">
+ <image source="../../tiles/walls.png" trans="000000" width="548" height="161"/>
+</tileset>
